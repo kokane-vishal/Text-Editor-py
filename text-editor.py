d306e880504e30aa6dc19da6a3dc505d2b2023c3
Vishal Kokane <88375109+KokaneVishal@users.noreply.github.com>
2023-11-30 08:21:49 +0530

    Add files via upload

---
+++ text-editor.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from tkinter import filedialog
+
+
+class TextEditor:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Text Editor")
+
+        self.text_widget = tk.Text(root, wrap="word", undo=True)
+        self.text_widget.pack(expand="yes", fill="both")
+
+        self.menu_bar = tk.Menu(root)
+        self.root.config(menu=self.menu_bar)
+
+        # File menu
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="New", command=self.new_file)
+        self.file_menu.add_command(label="Open", command=self.open_file)
+        self.file_menu.add_command(label="Save", command=self.save_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.root.quit)
+
+        # Edit menu
+        self.edit_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+        self.edit_menu.add_command(label="Cut", command=self.cut_text)
+        self.edit_menu.add_command(label="Copy", command=self.copy_text)
+        self.edit_menu.add_command(label="Paste", command=self.paste_text)
+
+    def new_file(self):
+        self.text_widget.delete(1.0, tk.END)
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(
+            defaultextension=".txt",
+            filetypes=[("Text files", "*.txt"), ("All files", "*.*")],
+        )
+        if file_path:
+            with open(file_path, "r") as file:
+                content = file.read()
+                self.text_widget.delete(1.0, tk.END)
+                self.text_widget.insert(1.0, content)
+
+    def save_file(self):
+        file_path = filedialog.asksaveasfilename(
+            defaultextension=".txt",
+            filetypes=[("Text files", "*.txt"), ("All files", "*.*")],
+        )
+        if file_path:
+            with open(file_path, "w") as file:
+                content = self.text_widget.get(1.0, tk.END)
+                file.write(content)
+
+    def cut_text(self):
+        self.text_widget.event_generate("<<Cut>>")
+
+    def copy_text(self):
+        self.text_widget.event_generate("<<Copy>>")
+
+    def paste_text(self):
+        self.text_widget.event_generate("<<Paste>>")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    text_editor = TextEditor(root)
+    root.mainloop()
